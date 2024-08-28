778a9273bae9ffa5e87aa759d009c6ad12166d08
Mohit Sharma <mohit.vsht@gmail.com>
2024-08-28 14:42:20 +0530

    backend redirect

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def index(request):
     # return render(request, 'index.html')
-    return HttpResponse("""
-        <h1 style="text-align:center"> Server Running </h1>
-    """)
+    # return HttpResponse("""
+    #     <h1 style="text-align:center"> Server Running </h1>
+    # """)
+    return redirect("https://musicappproject-gold.vercel.app")
 
 def indexRepath(request):
     return render(request, 'index.html')
